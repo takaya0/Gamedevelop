9ec5f4f690ccf8dfd0b469df03e164eb0b799462
takaya0 <ytk.koizumi@gmail.com>
2021-02-27 01:23:18 +0900

    first commit

---
+++ ブロック崩し/Assets/Scripts/Game_Master.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Game_Master : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int blocknum;
+    public float nowTime;
+    void Start()
+    {
+        nowTime = 0;
+        blocknum = GameObject.FindGameObjectsWithTag("block").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowTime += Time.deltaTime;
+        if (blocknum <= 0)
+        {
+            GameOver();
+        }
+    }
+    public void GameOver()
+    {
+        SceneManager.LoadScene("Result");
+    }
+
+}
---
+++ ブロック崩し/Assets/Scripts/block_init.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block_init : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject blockobjprefub;
+    public GameObject blocks;
+    void Awake()
+    {
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                GameObject g = Instantiate(blockobjprefub, blocks.transform);
+                g.transform.transform.position = new Vector3(2f + 1f * y, 0.4f, -4.2f + 1.2f * x);
+            }
+        }
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
